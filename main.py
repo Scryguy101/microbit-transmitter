d24957a263ec612340bfdfc64e52d8db5cd404e1
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-12-03 10:03:30 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
 
 #compass
 input.calibrate_compass()
-compass = input.compass_heading()
 
 def on_forever():
-    radio.send_number(input.acceleration(Dimension.Z))
-    radio.send_number(input.acceleration(Dimension.Y))
-    radio.send_number(input.acceleration(Dimension.X))
+    radio.send_value("Z", input.acceleration(Dimension.Z))
+    radio.send_value("Y", input.acceleration(Dimension.Y))
+    radio.send_value("X", input.acceleration(Dimension.X))
+    compass = input.compass_heading()
     radio.send_string(str(compass))
     basic.show_string(str(compass))
 basic.forever(on_forever)
@@ -18,6 +18,3 @@ basic.forever(on_forever)
 
 
  
-
-
-
