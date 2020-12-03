9cd47c3eadb50e1b7c6dd6992030d78acde1631a
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-12-03 09:39:37 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ radio.set_group(125)
 basic.show_string("SENDER")
 input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
 
+#compass
+input.calibrate_compass()
+compass = input.compass_heading()
+
 def on_forever():
     radio.send_number(input.acceleration(Dimension.Z))
     radio.send_number(input.acceleration(Dimension.Y))
     radio.send_number(input.acceleration(Dimension.X))
-
+    radio.send_string(str(compass))
 basic.forever(on_forever)
-
 
 
 
--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 radio.setGroup(125)
 basic.showString("SENDER")
 input.setAccelerometerRange(AcceleratorRange.EightG)
+// compass
+input.calibrateCompass()
+let compass = input.compassHeading()
 basic.forever(function on_forever() {
     radio.sendNumber(input.acceleration(Dimension.Z))
     radio.sendNumber(input.acceleration(Dimension.Y))
     radio.sendNumber(input.acceleration(Dimension.X))
+    radio.sendString("" + compass)
 })
